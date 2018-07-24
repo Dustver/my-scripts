e78764ad3b0488cae2c6001254fb8fd0574eaba8
Dustver <dustver@gmail.com>
2018-07-24 22:36:49 +0300

    подправил float в диаметре из сечения

--- diameter-transverse-section.py
+++ diameter-transverse-section.py
@@ -20,20 +20,12 @@ while True:
 1)диаметр исходя из сечения\n\
 2)сечение исходя из диаметра\n> "))
     if sel1 == 1:
-        while True:
-            s = input("Введите величину сечения(мм): ")
-            if s.isdigit():
-                s = int(s)
-                break
-        print(diametr(s))
+        s = float(input("Введите величину сечения(мм^2): "))
+        print("Диаметр равен %.2f мм" %diametr(s))
         break
     elif sel1 == 2:
-        while True:
-            d = input("Введите диаметр(мм): ")
-            if d.isdigit():
-                d = int(d)
-                break
-        print(transverse_section(d))
+        d = float(input("Введите диаметр(мм): "))
+        print("Поперечное сечение равно %.2f мм^2" %transverse_section(d))
         break
     else:
         print ("Ошибка ввода. Попробуйте ещё раз.")
